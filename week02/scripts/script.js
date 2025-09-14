b63cdee2cf0ccb0ded2e12a478ca58c58cc6f9d9
Gualter dos Santos <franciscobusiness30@gmail.com>
2025-09-14 22:52:54 +0300

    add code do add an item to a list and to remove item

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -1,12 +1,25 @@
-
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
 
-li.append(deleteButton);
-list.append(li);
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+});
